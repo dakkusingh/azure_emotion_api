9cbffa626117a8ae7f09699029030ef01a671933
dakku <dakku@97634.no-reply.drupal.org>
2017-09-21 23:42:22 +0100

    PHPCS

--- src/Service/Emotion.php
+++ src/Service/Emotion.php
@@ -5,6 +5,9 @@ namespace Drupal\azure_emotion_api\Service;
 use Drupal\azure_cognitive_services_api\Service\Client;
 use Drupal\Core\Config\ConfigFactory;
 
+/**
+ *
+ */
 class Emotion {
 
   const API_URL = '/emotion/v1.0/';
@@ -16,15 +19,23 @@ class Emotion {
     $this->client = new Client($config_factory, 'emotion');
   }
 
-  // See https://westus.dev.cognitive.microsoft.com/docs/services/5639d931ca73072154c1ce89/operations/56f23eb019845524ec61c4d7
-
+  /**
+   * See https://westus.dev.cognitive.microsoft.com/docs/services/5639d931ca73072154c1ce89/operations/56f23eb019845524ec61c4d7.
+   */
   public function recognize($photoUrl) {
     $uri = self::API_URL . 'recognize';
     $result = $this->client->doRequest($uri, 'POST', ['url' => $photoUrl]);
     return $result;
   }
 
+  /**
+   *
+   */
   public function recognizeEmotionRectangles() {}
+
+  /**
+   *
+   */
   public function recognizeInVideo() {}
 
 }
